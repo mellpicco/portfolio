b1de2e5c74743352a641cd7a80a2e470542b0237
mellpicco <39464414+mellpicco@users.noreply.github.com>
2022-06-01 17:44:41 -0400

    Update scripts.js
    adding js comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
 console.log("Hello world")
+
+
+
+
+/* multiline comment
+
+var text1 = "Hello";
+var escapedText1 = "He said: \"Yes!\", that's for sure";
+
+var numberInteger = 456;
+var numberInteger2 = -345; // This is negative integer
+var numberFloat = 50.25;
+
+*/
+
+// single line comment //
