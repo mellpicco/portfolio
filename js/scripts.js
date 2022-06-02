e2657f52dbfe52f327f9530b1b9da6fcb9043b17
mellpicco <39464414+mellpicco@users.noreply.github.com>
2022-06-02 00:22:39 -0400

    Update scripts.js
    js examples added to comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,56 @@
+
+
+var escapedText1 = "He said: \"Yes!\", that's for sure";
+var text2 = "Hello, spaces are also OK!";
+var numberInteger = 456;
+var emptyVariable = null;
+
+
+
+
 console.log("Hello world")
-
-
-
+console.log(escapedText1,text2, numberInteger)
+console.log(thisVariableDoesntExist); // Will output undefined
+console.log(emptyVariable); // Outputs Null
 
 /* multiline comment
 
+Examples of Strings
 var text1 = "Hello";
-var escapedText1 = "He said: \"Yes!\", that's for sure";
+var text2 = "Hello, spaces are also OK!";
+var text3 = 'Single quotes are valid too.';
 
+Examples of escaping with \ \
+var escapedText1 = "He said: \"Yes!\", that's for sure";
+var escapedText2 = 'He said: "Yes!", that\'s for sure';
+
+Examples of Numbers/Integers
 var numberInteger = 456;
 var numberInteger2 = -345; // This is negative integer
 var numberFloat = 50.25;
+
+Examples of Boolean
+var boolean1 = true;
+var boolean2 = false;
+var notABoolean = "false"; //This is just the string "false", not false!
+
+
+Undefined
+console.log(thisVariableDoesntExist); // Will output undefined
+
+Null
+var emptyVariable = null;
+var notNullButAString = "null";
+console.log(doesntExist); // Outputs undefined
+console.log(emptyVariable); // Outputs Null
+console.log(notNullButAString); // Outputs "null" - not the same as null!
+
+defining an object:
+var person = {
+  name: "John",
+  age: 39
+};
+
 
 */
 
