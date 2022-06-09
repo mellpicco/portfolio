e760eb65321c42d249718c9cc815113a8cb7210e
mellpicco <39464414+mellpicco@users.noreply.github.com>
2022-06-08 22:39:32 -0400

    Smooth Scrolling Added
    Smooth scrolling style added to style.css
    cross browser support for smooth scroll added to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@
 
 
 
-
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
@@ -11,6 +10,36 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 }
+
+
+
+  // Smooth scrolling for cross browser compatibility//
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
 
 
 
